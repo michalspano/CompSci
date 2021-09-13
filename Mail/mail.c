089016752b51ceb002a1f9582727e3f9bb880dc6
michalspano <speko.spano@gmail.com>
2021-09-13 20:35:18 +0200

    Add Mail/ in C;

---
+++ Mail/mail.c
@@ -0,0 +1,106 @@
+// Include libraries
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Define macros
+#define DOMAIN "gmail.com"
+#define ERROR "Usage: ./mail $INPUT_PATH $OUTPUT_PATH\n"
+
+// Declare function prototypes
+int fileError(char * PATH);
+void writeDomain(FILE *ip);
+
+// Using command-line arguments to parse input
+int main(int argc, char *argv[])
+{
+    // Check for number of command-line arguments (expected 3)
+    if (argc != 3)
+    {
+        fprintf(stderr, ERROR);
+        return 1;
+    }
+
+    // Open a text files as a file pointer of type 'FILE'
+    FILE *inputTextFile = fopen(argv[1], "r");
+    FILE *outputTextFile = fopen(argv[2], "w");
+
+    // Check for any errors
+    if (inputTextFile == NULL)
+    {
+        return fileError(argv[1]);
+    }
+
+    if (outputTextFile == NULL)
+    {
+        return fileError(argv[2]);
+    }
+
+    // Declare a buffer of type char (read single char at once)
+    char buffer;
+
+    // Declare a temp string
+    char temp[200];
+
+    // Read char by char from a text file 'fgetc'
+    int i = 0;
+    while ((buffer = fgetc(inputTextFile)) != EOF)
+    {
+        // Assign individual chars to a temp string
+        temp[i] = buffer;
+        i++;
+    }
+
+    /* 
+    Each string is denoted by '\n' at the end of each line
+    */
+
+    // Iterate over all chars in the temp. string    
+    for (int i = 0, n = strlen(temp); i < n; i++)
+    {
+        // Transform all chars to lower-case
+        char chr = tolower(temp[i]);
+
+        // Replace ';' with '.' -> proper mail structure
+        if (chr == ';') {chr = '.';}
+
+        // Detect '\n' in the following index and asssign respective domain from macros
+        if (temp[i+1] == '\n')
+        {
+            // Write to a text 
+            writeDomain(outputTextFile);
+        }
+
+        // Otherwise print out remaining chars
+        else
+        {
+            // Write to a text file
+            fputc(chr, outputTextFile);
+        }
+    }
+
+    // Print out domain for the last row (once EOF is met) -> break
+    writeDomain(outputTextFile);
+
+    // Close text file
+    fclose(inputTextFile);
+    fclose(outputTextFile);
+
+    // Return 0 after successful completion
+    return 0;
+}
+
+// Create a function to output an error at a given path
+int fileError(char * PATH)
+{
+    fprintf(stderr, "%s not available.\n", PATH);
+    return 1;
+}
+
+// Create a function to assign each domain
+void writeDomain(FILE *ip)
+{
+    // Write to a text
+    fputc('@', ip);
+    fputs(DOMAIN, ip);
+}
