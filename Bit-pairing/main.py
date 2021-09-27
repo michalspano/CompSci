4b1f8f0f38465678a4f27436d8766b282154b0fa
michalspano <speko.spano@gmail.com>
2021-09-27 19:38:20 +0200

    Update bit-pairing/

--- Bit-pairing/main.py
+++ Bit-pairing/main.py
@@ -27,17 +27,21 @@ def main(PATH):
     column_sequence(table)
 
     # Compute diagonal sequence from input
-    diagonal_sequence(table)
+    p_o: tuple = diagonal_sequence(table)  # {Parity occurrence}
+    output_parity: list = compute_diagonal_parity(p_o[0], p_o[1])
 
     """
     Nested loop comprehension (structure):
     element for sublist in list for element in sublist ('element' is to be assigned)
     [Firstly, parse sublist and then iter in sublist]
     """
+    print("*** BIT PARITY ***\n'True' - Sequence has bit parity\n'False' - Invalid bit parity\n")
 
     # Print out parsed data
-    print(f"Rows and columns:\n\n{' '.join(str(p) for sublist in parity for p in sublist)}")
-    print(f"Diagonals:\n\n{' '.join(str(p) for sublist in diagonal_parity for p in sublist)}")
+    print(f"Rows and columns:\n{' '.join(str(p) for sublist in parity for p in sublist)}")
+    print(f'{len(parity)}x{len(parity)}\n')
+    print(f"Diagonals:\n{' '.join(str(p) for sublist in output_parity for p in sublist)}")
+    print(f'{len(output_parity)}x{len(output_parity)}')
 
 
 # Read input to a nested list
@@ -79,25 +83,15 @@ def column_sequence(input_sequence: list):
 
 
 # Define a function to compute diagonal values
-def diagonal_sequence(input_sequence: list):
-    global diagonal_parity  # {Assigning type bool or None}
+def diagonal_sequence(input_sequence: list) -> tuple:
 
     """
     Parity determined by at least 2 elements, otherwise type None
     """
+    diminishing_diagonal: list = []  # {Compute for the first section}
+    increasing_diagonal: list = []  # {Compute for the second section}
 
     # Section 1 (diminishing line)
-    for k in range(len(input_sequence) - 1):
-        n: int = 0
-        counter: int = 0
-        for j in range(k, len(input_sequence)):
-            value = input_sequence[j][n]
-            if value == '1':
-                counter += 1
-            n += 1
-        state = parity_check(counter)  # {Invoke parity evaluation}
-        diagonal_parity[k][0] = state
-
     for k in range(1, len(input_sequence) - 1):
         i: int = k
         counter: int = 0
@@ -107,22 +101,25 @@ def diagonal_sequence(input_sequence: list):
                 counter += 1
             i += 1
         state = parity_check(counter)  # {Invoke parity evaluation}
-        diagonal_parity[k + len(input_sequence) - 1][0] = state
+        diminishing_diagonal.append(state)
 
-    # Section 2 (increasing line)
-    k = len(input_sequence) - 1
-    while k >= 1:
-        i: int = len(input_sequence) - (k + 1)
+    # Refactor to the correct order (starting upwards)
+    diminishing_diagonal.append(None)
+    diminishing_diagonal = diminishing_diagonal[::-1]
+
+    for k in range(len(input_sequence) - 1):
+        n: int = 0
         counter: int = 0
-        for j in range(len(input_sequence) - 1, len(input_sequence) - k - 2, -1):
-            value = input_sequence[j][i]
+        for j in range(k, len(input_sequence)):
+            value = input_sequence[j][n]
             if value == '1':
                 counter += 1
-            i += 1
-        state = parity_check(counter)
-        diagonal_parity[len(input_sequence) - (k + 1)][1] = state
-        k -= 1
+            n += 1
+        state = parity_check(counter)  # {Invoke parity evaluation}
+        diminishing_diagonal.append(state)
+    diminishing_diagonal.append(None)
 
+    # Section 2 (increasing line)
     k = len(input_sequence) - 2
     while k >= 1:
         i: int = 0
@@ -133,13 +130,45 @@ def diagonal_sequence(input_sequence: list):
                 counter += 1
             i += 1
         state = parity_check(counter)
-        diagonal_parity[len(input_sequence) + (len(input_sequence) - 2 - k)][1] = state
+        increasing_diagonal.append(state)
         k -= 1
+
+    # Refactor to the correct order (starting upwards)
+    increasing_diagonal.append(None)
+    increasing_diagonal = increasing_diagonal[::-1]
+
+    k = len(input_sequence) - 1
+    while k >= 1:
+        i: int = len(input_sequence) - (k + 1)
+        counter: int = 0
+        for j in range(len(input_sequence) - 1, len(input_sequence) - k - 2, -1):
+            value = input_sequence[j][i]
+            if value == '1':
+                counter += 1
+            i += 1
+        state = parity_check(counter)
+        increasing_diagonal.append(state)
+        k -= 1
+    increasing_diagonal.append(None)
+
+    # Return both lists of occurrence
+    return diminishing_diagonal, increasing_diagonal
 
 
 # Function to evaluate parity
 def parity_check(count: int) -> bool:
     return True if count % 2 == 0 else False
+
+
+# Function to evaluate and format diagonal parity
+def compute_diagonal_parity(d1: list, d2: list) -> list:
+    global diagonal_parity  # {Global reference}
+    for i in range(len(d1)):  # {Iterate over all instances}
+        diagonal_parity[i][0] = d1[i]
+        diagonal_parity[i][1] = d2[i]
+
+    # Format 2 lists to a nested list
+    return diagonal_parity
 
 
 # Invoke main function
