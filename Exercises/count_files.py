3457ea4f35592001933ef7f5e615d6033b1dc266
michalspano <speko.spano@gmail.com>
2022-05-02 19:46:27 +0200

    Add a file counter

---
+++ Exercises/count_files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from os import listdir
+
+
+def main():
+    buff: list = []
+
+    for filename in listdir('.'):
+        if filename.isdigit():  # assign only digits to buffer
+            buff.append(int(filename))  
+    buff.sort()  # sort to ascending order
+
+    _ = open('sorted.txt', 'w').write(','.join(map(str, buff)))  # write sorted numbers to file
+
+    print(f'{len(buff)} Python files found.')  # print number of files found
+
+
+if __name__ == '__main__':
+    main()
