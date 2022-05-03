17820e4554a4155cc64d7b2c4ecf59e268345cde
michalspano <speko.spano@gmail.com>
2022-05-03 20:00:47 +0200

    Automate file `counting`

--- Exercises/count_files.py
+++ Exercises/count_files.py
@@ -6,12 +6,13 @@ from os import listdir
 def main():
     buff: list = []
 
-    for filename in listdir('.'):
-        if filename.isdigit():  # assign only digits to buffer
-            buff.append(int(filename))  
+    for name in listdir():
+        if name.isdigit():  # assign only digits to buffer
+            buff.append(int(name))  
+
     buff.sort()  # sort to ascending order
 
-    _ = open('sorted.txt', 'w').write(','.join(map(str, buff)))  # write sorted numbers to file
+    _ = open('sorted.txt', 'w').write(', '.join(map(str, buff)))  # write sorted numbers to file
 
     print(f'{len(buff)} Python files found.')  # print number of files found
 
