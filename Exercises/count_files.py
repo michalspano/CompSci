84fe62f4f68b1d93e8dc28fa9fd64c282634376c
michalspano <speko.spano@gmail.com>
2022-05-06 21:18:33 +0200

    Add new functionality: `compute random exercise` from `CLI`

--- Exercises/count_files.py
+++ Exercises/count_files.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 
+# Import modules
 from os import listdir
+from random import randint, choice
+from sys import argv
 
 
+# Define the main function
 def main():
+    # Check usage, if no arguments are passed, show usage
+    if len(argv) != 2:
+        print('Usage: ./count_files.py <operand>')
+        return
+    
+    # Parse the operand
+    operand: str = str(argv[1]).lower()
+    
+    # Check the operand and execute the corresponding function
+    if operand == '--read' or operand == '-r':
+        read_dir(), exit()
+        
+    elif operand == '--show' or operand == '-s':
+        print(f'Randomly chosen exercise: {show_random_int()}'), exit()
+
+    print('Unknown operand.')
+
+
+def read_dir(_dir: str = './') -> None:
     buff: list = []
 
-    for name in listdir():
+    for name in listdir(_dir):
         if name.isdigit():  # assign only digits to buffer
             buff.append(int(name))  
 
@@ -15,6 +38,11 @@ def main():
     _ = open('sorted.txt', 'w').write(', '.join(map(str, buff)))  # write sorted numbers to file
 
     print(f'{len(buff)} Python files found.')  # print number of files found
+
+
+# Compute a random number from the interval of parsed numbers
+def show_random_int(inp: str = 'sorted.txt') -> int:
+    return choice(open(inp).read().split(', '))  # return random number from file
 
 
 if __name__ == '__main__':
